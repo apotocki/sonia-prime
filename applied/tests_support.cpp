198253b6ab802b53ed389118fc40db78213ee68e
apotocki <alexander.pototskiy@ai4bd.com>
2019-07-08 16:49:35 +0200

    fix

--- applied/tests_support.cpp
+++ applied/tests_support.cpp
@@ -3,6 +3,7 @@
 //  Sonia.one is licensed under the terms of the Open Source GPL 3.0 license.
 //  For a license to use the Sonia.one software under conditions other than those described here, please contact me at admin@sonia.one
 //
+
 #ifndef BOOST_TEST_MODULE
 #   define BOOST_TEST_MODULE sonia_regression_tests
 #endif

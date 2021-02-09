5c80792ab4e6cd57c765156c024a4732e6b1f5cd
apotocki <sane22222@aim.com>
2021-02-09 22:15:52 +0100

    minor

--- sonia/utility/scope_exit.hpp
+++ sonia/utility/scope_exit.hpp
@@ -4,12 +4,7 @@
 
 // impl based on https://stackoverflow.com/questions/3669833/c11-scope-exit-guard-a-good-idea
 
-#ifndef SONIA_SCOPE_EXIT_HPP
-#define SONIA_SCOPE_EXIT_HPP
-
-#ifdef BOOST_HAS_PRAGMA_ONCE
-#   pragma once
-#endif
+#pragma once
 
 #include <utility>
 #include <boost/exception/diagnostic_information.hpp>
@@ -52,5 +47,3 @@ scope_exit<T> scope_exit_create(T&& exitScope)
 #define SONIA_UTILITY_EXIT_SCOPE_LINENAME_CAT(name, line) name##line
 #define SONIA_UTILITY_EXIT_SCOPE_LINENAME(name, line) SONIA_UTILITY_EXIT_SCOPE_LINENAME_CAT(name, line)
 #define SCOPE_EXIT(...) const auto& SONIA_UTILITY_EXIT_SCOPE_LINENAME(EXIT, __LINE__) = ::sonia::scope_exit_create(__VA_ARGS__)
-
-#endif // SONIA_SCOPE_EXIT_HPP

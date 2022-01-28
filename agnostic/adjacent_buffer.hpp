f3cab738d76e9bb5d7c58c1cfd63341c7f8af3ed
apotocki <sane22222@aim.com>
2022-01-28 14:28:01 +0100

    moved to traits approach

--- agnostic/adjacent_buffer.hpp
+++ agnostic/adjacent_buffer.hpp
@@ -36,30 +36,63 @@
 
 namespace agnostic {
 
-template <typename T, typename BaseT>
-struct adjacent_buffer : BaseT
+/*
+struct Traits
+{
+    using value_type = unspecified_type;
+    using size_type = unspecified_type;
+    using base_t = unspecified_type;
+    static constexpr size_type first = ...;
+    static inline void const* first(base_t const&) { ... }
+    static inline size_type size(base_t const&) { ... }
+    static inline void set_size(base_t &, size_type) { ... }
+    static void destroy(base_t&) noexcept { ... }
+};
+*/
+
+template <typename T, typename BaseT, typename SizeT = size_t>
+struct basic_adjacent_buffer_traits
 {
     using value_type = T;
-    using size_type = size_t;
-    static constexpr size_t type_alignment = alignof(T);
+    using size_type = SizeT;
+
+    struct base_t : BaseT {
+        size_type size_;
+        alignas(T) char first_[sizeof(T)];
+
+        template <typename ... ArgsT>
+        explicit base_t(size_type sz, ArgsT&& ... args) : BaseT(std::forward<ArgsT>(args)), size_{ sz } {}
+    };
+
+    static constexpr size_type first = offsetof(base_t, first_);
+    static inline size_type size(base_t const& self) noexcept { return self.size_; }
+    static inline void destroy(base_t&) noexcept { }
+};
+
+template <typename TraitsT>
+struct adjacent_buffer : TraitsT::base_t
+{
+    using value_type = typename TraitsT::value_type;
+    using size_type = typename TraitsT::size_type;
+    using base_t = typename TraitsT::base_t;
+    static constexpr size_t type_alignment = alignof(value_type);
 
     template <typename ... ArgsT>
     adjacent_buffer(uninitialized_t, size_type sz, ArgsT&& ... args)
-        : BaseT{std::forward<ArgsT>(args) ...}, sz_(sz)
+        : base_t{sz, std::forward<ArgsT>(args) ...}
     {
-
     }
 
     template <typename ... ArgsT>
     adjacent_buffer(default_constructed_t, size_type sz, ArgsT&& ... args)
-        : BaseT(std::forward<ArgsT>(args) ...), sz_(sz)
+        : base_t(sz, std::forward<ArgsT>(args) ...)
     {
         std::uninitialized_default_construct(begin(), end());
     }
 
     template <typename RngT, typename ... ArgsT>
     adjacent_buffer(RngT&& rng, size_type sz, ArgsT&& ... args)
-        : BaseT(std::forward<ArgsT>(args) ...), sz_(sz)
+        : base_t(sz, std::forward<ArgsT>(args) ...)
     {
         std::uninitialized_copy(std::begin(rng), std::end(rng), begin());
     }
@@ -71,46 +104,42 @@ struct adjacent_buffer : BaseT
 
     ~adjacent_buffer() noexcept
     {
-        BaseT::destroy();
+        TraitsT::destroy(*this);
     }
+    
+    inline value_type* begin() noexcept { return std::launder(reinterpret_cast<value_type*>(reinterpret_cast<uint8_t*>(this) + TraitsT::first)); }
+    inline value_type const* begin() const noexcept { return std::launder(reinterpret_cast<value_type const*>(reinterpret_cast<uint8_t const*>(this) + TraitsT::first)); }
+    inline value_type const* cbegin() const noexcept { return begin(); }
 
-    T* begin() noexcept { return std::launder(reinterpret_cast<T*>(first_)); }
-    T const* begin() const noexcept { return std::launder(reinterpret_cast<const T*>(first_)); }
-    T const* cbegin() const noexcept { return begin(); }
+    inline value_type* end() noexcept { return begin() + capacity(); }
+    inline value_type const* end() const noexcept { return begin() + capacity(); }
+    inline value_type const* cend() const noexcept { return end(); }
 
-    T* end() noexcept { return begin() + sz_; }
-    T const* end() const noexcept { return begin() + sz_; }
-    T const* cend() const noexcept { return end(); }
+    inline value_type& front() noexcept { return *begin(); }
+    inline value_type const& front() const noexcept { return *begin(); }
 
-    T& front() noexcept { return *begin(); }
-    T const& front() const noexcept { return *begin(); }
+    inline value_type& back() noexcept { return *(end() - 1); }
+    inline value_type const& back() const noexcept { return *(end() - 1); }
 
-    T& back() noexcept { return *(end() - 1); }
-    T const& back() const noexcept { return *(end() - 1); }
-
-    T& operator[](size_t ind) noexcept { return begin()[ind]; }
-    T const& operator[](size_t ind) const noexcept { return begin()[ind]; }
+    inline value_type& operator[](size_t ind) noexcept { return begin()[ind]; }
+    inline value_type const& operator[](size_t ind) const noexcept { return begin()[ind]; }
 
 
-    inline size_type capacity() const noexcept { return sz_; }
-    inline bool empty() const noexcept { return !sz_; }
+    inline size_type capacity() const noexcept { return TraitsT::size(*this); }
+    inline bool empty() const noexcept { return !capacity(); }
     inline explicit operator bool() const noexcept { return !empty(); }
-
-private:
-    size_type sz_;
-    alignas(T) char first_[sizeof(T)];
 };
 
-template <typename T, typename BaseT, BareAllocator AllocatorT, typename ... ArgsT>
-[[nodiscard]] adjacent_buffer<T, BaseT>* allocate_adjacent_buffer(AllocatorT alloc, size_t sz, ArgsT&& ... args)
+template <typename TraitsT, BareAllocator AllocatorT, typename ... ArgsT>
+[[nodiscard]] adjacent_buffer<TraitsT>* allocate_adjacent_buffer(AllocatorT alloc, typename TraitsT::size_type sz, ArgsT&& ... args)
 {
     assert(sz);
 
-    using buffer_t = adjacent_buffer<T, BaseT>;
-
+    using buffer_t = adjacent_buffer<TraitsT>;
+    constexpr size_t buffsz = sizeof(buffer_t);
     //const size_t allocsz = offsetof(buffer_t, first_) + sizeof(T) * sz;
-    const size_t allocsz = sizeof(buffer_t) + sizeof(T) * (sz - 1);
-
+    const size_t allocsz = (std::max)(buffsz, typename TraitsT::first + sizeof(typename TraitsT::value_type) * sz);
+    
     buffer_t* ptr = reinterpret_cast<buffer_t*>(alloc.allocate((std::align_val_t)alignof(buffer_t), allocsz));
 
     try {
@@ -121,12 +150,12 @@ template <typename T, typename BaseT, BareAllocator AllocatorT, typename ... Arg
     }
 }
 
-template <BareAllocator AllocatorT, typename T, typename BaseT>
-void deallocate_adjacent_buffer(AllocatorT alloc, adjacent_buffer<T, BaseT>* ptr) noexcept
+template <BareAllocator AllocatorT, typename TraitsT>
+void deallocate_adjacent_buffer(AllocatorT alloc, adjacent_buffer<TraitsT>* ptr) noexcept
 {
-    using buffer_t = adjacent_buffer<T, BaseT>;
+    using buffer_t = adjacent_buffer<TraitsT>;
     //const size_t allocsz = offsetof(buffer_t, first_) + sizeof(T) * ptr->capacity();
-    const size_t allocsz = sizeof(buffer_t) + sizeof(T) * (ptr->capacity() - 1);
+    const size_t allocsz = sizeof(buffer_t) + sizeof(typename TraitsT::value_type) * (ptr->capacity() - 1);
 
     std::destroy_at(ptr);
     alloc.deallocate(reinterpret_cast<char*>(ptr), allocsz);

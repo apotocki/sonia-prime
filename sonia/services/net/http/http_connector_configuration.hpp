c5b3b2c2b466a87f9a15c312edf4f7fb37ab8156
apotocki <sane22222@aim.com>
2022-12-10 17:03:03 +0100

    minor

--- sonia/services/net/http/http_connector_configuration.hpp
+++ sonia/services/net/http/http_connector_configuration.hpp
@@ -2,12 +2,7 @@
 //  Sonia.one is licensed under the terms of the Open Source GPL 3.0 license.
 //  For a license to use the Sonia.one software under conditions other than those described here, please contact me at admin@sonia.one
 
-#ifndef SONIA_SERVICES_HTTP_CONNECTOR_CONFIGURATION_HPP
-#define SONIA_SERVICES_HTTP_CONNECTOR_CONFIGURATION_HPP
-
-#ifdef BOOST_HAS_PRAGMA_ONCE
-#   pragma once
-#endif
+#pragma once
 
 #include <vector>
 #include "sonia/optional.hpp"
@@ -33,5 +28,3 @@ struct http_connector_configuration
 };
 
 }
-
-#endif // SONIA_SERVICES_HTTP_CONNECTOR_CONFIGURATION_HPP

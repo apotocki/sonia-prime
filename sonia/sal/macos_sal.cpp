93175ea634ec21508fc12addc3ab4e6a5a53d5ad
Alexander Pototskiy <alex.a.potocki@gmail.com>
2023-02-01 00:21:11 +0100

    dev

--- sonia/sal/macos_sal.cpp
+++ sonia/sal/macos_sal.cpp
@@ -4,12 +4,20 @@
 
 #include "sonia/config.hpp"
 #include "sonia/sal.hpp"
+#include "sys/statvfs.h"
 
 namespace sonia::sal {
 
 void get_disk_info(u8string_view path, disk_info& di)
 {
+    struct statvfs s;
+    if (-1 == statvfs((const char*)path.data(), &s)) {
+        int err = errno;
+        throw exception("get file size error : ", strerror(err));
+    }
 
+    di.total_size = (int64_t)s.f_frsize * (int64_t)s.f_blocks;
+    di.free_size = (int64_t)s.f_frsize * (int64_t)s.f_bfree;
 }
 
 }

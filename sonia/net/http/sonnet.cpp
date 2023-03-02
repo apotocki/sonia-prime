8e4cb7829e9eb6ec56849c2d6c72e3b55bdf38f1
Alexander Pototskiy <alex.a.potocki@gmail.com>
2023-03-02 15:33:50 +0100

    dev

--- sonia/net/http/sonnet.cpp
+++ sonia/net/http/sonnet.cpp
@@ -3,7 +3,7 @@
 //  For a license to use the Sonia.one software under conditions other than those described here, please contact me at admin@sonia.one
 
 #include "sonia/config.hpp"
-#include "sonia/build_config.hpp"
+#include "build_config.hpp"
 #include "sonnet.hpp"
 #include "sonia/exceptions.hpp"
 
--- sonia/services/prime_bundle.cpp
+++ sonia/services/prime_bundle.cpp
@@ -2,7 +2,7 @@
 //  Sonia.one is licensed under the terms of the Open Source GPL 3.0 license.
 //  For a license to use the Sonia.one software under conditions other than those described here, please contact me at admin@sonia.one
 
-#include "sonia/build_config.hpp"
+#include "build_config.hpp"
 #include "sonia/prime_config.hpp"
 
 #include "prime_bundle.hpp"

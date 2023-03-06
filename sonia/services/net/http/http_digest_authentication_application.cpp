a84e59c69258ed28b35bb0061f5beefb57847c59
Alexander Pototskiy <alex.a.potocki@gmail.com>
2023-03-06 12:41:51 +0100

    dev

--- sonia/services/net/http/http_digest_authentication_application.cpp
+++ sonia/services/net/http/http_digest_authentication_application.cpp
@@ -331,7 +331,8 @@ void http_digest_authentication_application::handle(http::request & req, http::r
     }
 
     // skip request body if exists
-    //for (; !req.input.empty(); ++req.input);
+    req.input.close();
+    for (; !req.input.empty(); ++req.input);
     req.keep_alive = false;
     std::string nonce = get_nonce();
     resp.meet_request(req);

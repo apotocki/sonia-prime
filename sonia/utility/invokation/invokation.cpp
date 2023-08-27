668502ac7813feaca132a63ca5496bd1f0fc0e5d
apotocki <sane22222@aim.com>
2023-08-27 23:44:10 +0200

    invokation refactoring

--- sonia/utility/invokation/invokation.cpp
+++ sonia/utility/invokation/invokation.cpp
@@ -15,6 +15,13 @@ namespace sonia::invokation {
 class blob_manager : public singleton
 {
 public:
+    ~blob_manager()
+    {
+        if (!blobs_.empty()) {
+            GLOBAL_LOG_ERROR() << "BLOB LEAKS: " << blobs_.size();
+        }
+    }
+
     void pin(shared_ptr<uint8_t> pval)
     {
         lock_guard guard(blobs_mtx_);
@@ -29,6 +36,33 @@ public:
         }
     }
 
+    void addref(blob_result* b)
+    {
+        lock_guard guard(blobs_mtx_);
+        auto it = blobs_.find(b->data);
+        if (it != blobs_.end()) {
+            ++it->second.second;
+        } else {
+            GLOBAL_LOG_DEBUG() << "a blob was not found for pinning at: " << std::hex << (uint64_t)b->data;
+        }
+    }
+
+    void releaseref(blob_result* b) noexcept
+    {
+        lock_guard guard(blobs_mtx_);
+        auto it = blobs_.find(b->data);
+        if (it != blobs_.end()) {
+            if (--it->second.second == 0) {
+                blobs_.erase(it);
+                GLOBAL_LOG_TRACE() << "a blob was unpinned at: " << std::hex << (uint64_t)b->data;
+            }
+        } else {
+            GLOBAL_LOG_ERROR() << "a blob was not found for unpinning at: " << std::hex << (uint64_t)b->data;
+            //THROW_INTERNAL_ERROR("a blob was not found for unpinning %1%"_fmt % b);
+        }
+    }
+
+    /*
     void allocate(blob_result * b)
     {
         if (b->need_unpin == 0 && b->size > 0) {
@@ -88,6 +122,7 @@ public:
             }
         }
     }
+    */
 
 private:
     mutable fibers::mutex blobs_mtx_;
@@ -98,15 +133,45 @@ private:
 
 void blob_result_allocate(blob_result * b)
 {
-    sonia::as_singleton<sonia::invokation::blob_manager>()->allocate(b);
+    if (b->need_unpin == 0 && b->size > 0) {
+        sonia::shared_ptr<uint8_t> data(new uint8_t[b->size], [](uint8_t* p) { delete[] p; });
+        if (b->data) {
+            std::memcpy(data.get(), b->data, b->size);
+        }
+        b->data = data.get();
+        b->need_unpin = 1;
+        sonia::as_singleton<sonia::invokation::blob_manager>()->pin(std::move(data));
+    }
 }
 
 void blob_result_pin(blob_result * b)
 {
-    sonia::as_singleton<sonia::invokation::blob_manager>()->pin(b);
+    using namespace sonia;
+    if (b->type == blob_type::blob || (b->is_array && b->type == blob_type::string)) {
+        blob_result* rdata = const_cast<blob_result*>(reinterpret_cast<blob_result const*>(b->data));
+        blob_result const* endrdata = reinterpret_cast<blob_result const*>(b->data) + b->size / sizeof(blob_result);
+        for (; rdata != endrdata; ++rdata) {
+            blob_result_pin(rdata);
+        }
+    }
+    if (b->need_unpin) {
+        sonia::as_singleton<sonia::invokation::blob_manager>()->addref(b);
+    }
 }
 
 void blob_result_unpin(blob_result * b)
 {
-    sonia::as_singleton<sonia::invokation::blob_manager>()->unpin(b);
+    if (b->type == blob_type::blob || (b->is_array && b->type == blob_type::string)) {
+        blob_result* rdata = const_cast<blob_result*>(reinterpret_cast<blob_result const*>(b->data));
+        blob_result const* endrdata = reinterpret_cast<blob_result const*>(b->data) + b->size / sizeof(blob_result);
+        for (; rdata != endrdata; ++rdata) {
+            blob_result_unpin(rdata);
+        }
+    }
+        
+    if (b->need_unpin) {
+        //GLOBAL_LOG_DEBUG() << "a blob to unpin: " << b;
+        b->need_unpin = 0;
+        sonia::as_singleton<sonia::invokation::blob_manager>()->releaseref(b);
+    }
 }

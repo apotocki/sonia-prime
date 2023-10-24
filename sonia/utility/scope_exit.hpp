5bfed3c27e0e4046a6dc8f4b4b8536230af4f4a1
apotocki <sane22222@aim.com>
2023-10-25 01:48:33 +0200

    fix

--- sonia/utility/scope_exit.hpp
+++ sonia/utility/scope_exit.hpp
@@ -47,4 +47,4 @@ scope_exit<T> scope_exit_create(T&& exitScope)
 #define SONIA_UTILITY_EXIT_SCOPE_LINENAME_CAT(name, line) name##line
 #define SONIA_UTILITY_EXIT_SCOPE_LINENAME(name, line) SONIA_UTILITY_EXIT_SCOPE_LINENAME_CAT(name, line)
 #define SCOPE_EXIT(...) const auto& SONIA_UTILITY_EXIT_SCOPE_LINENAME(EXIT, __LINE__) = ::sonia::scope_exit_create(__VA_ARGS__)
-#define defer const ::sonia::scope_exit SONIA_UTILITY_EXIT_SCOPE_LINENAME(EXIT, __LINE__) = [=]
+#define defer const ::sonia::scope_exit SONIA_UTILITY_EXIT_SCOPE_LINENAME(EXIT, __LINE__) = [&]

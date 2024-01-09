03f90b2093fd2f0f09ea0de14c2307cd547f2b2e
apotocki <sane22222@aim.com>
2024-01-09 17:25:55 +0100

    float16 subnormal values fix

--- sonia/utility/number/float16.hpp
+++ sonia/utility/number/float16.hpp
@@ -32,7 +32,7 @@ inline float16::float16(float f32val) noexcept
     uint32_t f32data = std::bit_cast<uint32_t>(f32val);
 #ifndef __F16CINTRIN_H
     const uint32_t frac32 = f32data & 0x7fffff;
-    const uint8_t exp32 = (f32data & 0x7f800000) >> 23;
+    const uint8_t exp32 = f32data >> 23;
     const int8_t exp32_diff = exp32 - 127;
 
     uint16_t exp16 = 0;
@@ -51,7 +51,7 @@ inline float16::float16(float f32val) noexcept
     } else if (!exp32 || (exp16 == 0x1f && exp32 != 0xff)) {
         frac16 = 0;
     } else if (!exp16 && exp32) {
-        frac16 = 0x100 | (frac16 >> 2);
+        frac16 = (0x200 | (frac16 >> 1)) >> (17 - exp32_diff);
     }
 
     data = (f32data & 0x80000000) >> 16; // sign
